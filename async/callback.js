4d4b4a3e4f1337dbe6e58189c0cf0a47eb369666
Edison Proano <clanuzumaki22@gmail.com>
2020-06-24 23:28:02 -0500

    Creacion de archivo callback.

---
+++ async/callback.js
@@ -0,0 +1,15 @@
+var Callback = (function() {
+
+    function Callback() {
+
+    }
+
+    Callback.prototype.soyAsincrona = function () {
+        console.log('Hola, soy una funcion asincrona');
+    };
+
+    return Callback;
+
+})
+
+export default Callback;
