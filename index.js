abb5ceb393e4fb7024a44806c2deb1ed7dc81bf7
Edison Proano <clanuzumaki22@gmail.com>
2020-06-24 23:27:34 -0500

    Creacion de funcion asincrona.

---
+++ index.js
@@ -0,0 +1,15 @@
+function soyAsincrona() {
+    console.log("Hola, soy una funcion asincrona");
+}
+
+soyAsincrona();
+
+
+
+/* import Callback from './async/callback.js';
+
+function exec () {
+    Callback.soyAsincrona();
+}
+
+exec(); */
