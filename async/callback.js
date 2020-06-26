15f17ad78d3312eb0e4658a577b038d6f338aad8
Edison Proano <clanuzumaki22@gmail.com>
2020-06-26 12:04:10 -0500

    Creacion de funciones con callbacks

--- async/callback.js
+++ async/callback.js
@@ -1,15 +1,21 @@
-var Callback = (function() {
+function Callback () {
 
-    function Callback() {
+}
+    
+Callback.prototype.hola = function(nombre, miCallback) {
+    setTimeout(function () {
+        console.log(`Hola, ${nombre}`);
+        miCallback(nombre);
+    }, 1000)
 
-    }
+Callback.prototype.adios = function(nombre, otroCallback) {
+    setTimeout(function () {
+        console.log(`Adios, ${nombre}`);
+        otroCallback();
+    }, 1500)
+}
 
-    Callback.prototype.soyAsincrona = function () {
-        console.log('Hola, soy una funcion asincrona');
-    };
 
-    return Callback;
+}
 
-})
-
-export default Callback;
+module.exports = Callback;
