dc9a1e324bffe229466f8ee962554e94913a8082
Edison Proano <clanuzumaki22@gmail.com>
2020-06-26 12:51:58 -0500

    Creacion de funciones callbackHell y solucion.

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 var Callback = require('./async/callback.js');
+var CallbackHell = require('./async/callbackHell.js');
 
-var callback = new Callback();
+// var callback = new Callback();
+var callbackHell = new CallbackHell();
 
 
-console.log(`Iniciando proceso....`);
+/* console.log(`Iniciando proceso....`);
 callback.hola('Edison', function() {
     callback.adios('Edison', function() {
         console.log(`Terminando proceso....`);
     })   
+}); */
+
+
+/* console.log(`Iniciando proceso....`);
+callbackHell.hola('Edison', function() {
+    callbackHell.hablar(function() {
+        callbackHell.hablar(function() {
+            callbackHell.hablar(function() {   
+                callbackHell.adios('Edison', function() {
+                    console.log(`Terminando proceso....`);
+                });
+            });
+        });  
+    });   
+}); */
+
+console.log(`Iniciando proceso....`);
+callbackHell.hola('Edison', function() {
+    callbackHell.conversacion('Edison',2, function() {
+        console.log('Proceso terminado');
+    })
 });
-
-
 
 
