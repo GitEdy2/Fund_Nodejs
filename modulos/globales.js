5c558e715feb442485e1c35a0a1c83a4b3812cf4
Edison Proano <clanuzumaki22@gmail.com>
2020-06-26 17:14:02 -0500

    Creacion de modulos con global.

---
+++ modulos/globales.js
@@ -0,0 +1,17 @@
+let i = 0;
+
+let intervalo = setInterval(() => {
+    console.log('Hola');
+    if (i === 3) {
+        clearInterval(intervalo);
+    }
+    i++
+}, 1000); */
+
+setImmediate(function() {
+    console.log('Hola');
+});
+
+console.log(process);
+
+console.log(__filename);
